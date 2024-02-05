414b86854450443d7696e767a6b4e4ab474b303d
kaps02 <155291269+kaps02@users.noreply.github.com>
2024-02-05 16:20:23 +0530

    write into react

---
+++ ExpenseItem.js
@@ -0,0 +1,19 @@
+const ExpenseItem =()=>{
+    return (
+        <div>
+            <p>
+            <div>
+                <h3>Expense Items</h3>
+            </div>
+
+           <h4> Food Rs 10</h4>
+
+           <h4> Petrol Rs 100</h4>
+           <div>
+            <h5> Movies Rs 200</h5>
+           </div>
+            </p>
+        </div>
+    )
+}
+export default ExpenseItem;
